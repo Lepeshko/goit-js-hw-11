9a03216fb86cb5217847d29bd8df548596876c27
Lepeshko <anatolii.lepeshko1@gmail.com>
2024-05-24 17:20:39 +0300

    commit-24.05.24-17.20

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,11 +1,7 @@
 'use strict';
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
-// Описаний у документації
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export function displayImages(images, gallery) {
